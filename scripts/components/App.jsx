e97d3d3ef09a5a2abe27fb8c3818b66595fd3fe3
Chad Scira <chadvscira@gmail.com>
2014-07-20 03:59:26 +0700

    removed unused code in App.jsx

--- scripts/components/App.jsx
+++ scripts/components/App.jsx
@@ -4,38 +4,10 @@
 var Backbone = require('backbone'),
 	React = require('react'),
 	Footer = require('./Footer'),
-	TodoItem = require('./TodoItem');
+	TodoItem = require('./TodoItem'),
+	BackboneMixin = require('./mixins/BackboneMixin');
 
 var ENTER_KEY = 13;
-
-// An example generic Mixin that you can add to any component that should
-// react to changes in a Backbone component. The use cases we've identified
-// thus far are for Collections -- since they trigger a change event whenever
-// any of their constituent items are changed there's no need to reconcile for
-// regular models. One caveat: this relies on getBackboneCollections() to
-// always return the same collection instances throughout the lifecycle of the
-// component. If you're using this mixin correctly (it should be near the top
-// of your component hierarchy) this should not be an issue.
-var BackboneMixin = {
-	componentDidMount: function () {
-		// Whenever there may be a change in the Backbone data, trigger a
-		// reconcile.
-		this.getBackboneCollections().forEach(function (collection) {
-			// explicitly bind `null` to `forceUpdate`, as it demands a callback and
-			// React validates that it's a function. `collection` events passes
-			// additional arguments that are not functions
-			collection.on('add remove change', this.forceUpdate.bind(this, null));
-		}, this);
-	},
-
-	componentWillUnmount: function () {
-		// Ensure that we clean up any dangling references when the component is
-		// destroyed.
-		this.getBackboneCollections().forEach(function (collection) {
-			collection.off(null, null, this);
-		}, this);
-	}
-};
 
 var App = React.createClass({
 	mixins: [BackboneMixin],
@@ -121,12 +93,15 @@ var App = React.createClass({
 	},
 
 	render: function () {
-		var footer;
-		var main;
-		var todos = this.props.todos;
+		var todos = this.props.todos,
+			main,
+			footer,
+			shownTodos,
+			todoItems,
+			activeTodoCount = 0,
+			completedCount = 0;
 
-		var shownTodos;
-
+		// do we have any todos?
 		if (todos) {
 			shownTodos = todos.filter(function (todo) {
 				if (this.state.nowShowing === 'active') {
@@ -137,11 +112,7 @@ var App = React.createClass({
 					return true;
 				}
 			}, this);
-		}
-
-		var todoItems;
-
-		if (shownTodos) {
+		
 			todoItems = shownTodos.map(function (todo) {
 				return (
 					<TodoItem
@@ -156,16 +127,10 @@ var App = React.createClass({
 					/>
 				);
 			}, this);
-		}
 
-		var activeTodoCount = 0,
-			completedCount = 0;
-
-		if (todos) {
 			activeTodoCount = todos.reduce(function (accum, todo) {
 				return todo.get('completed') ? accum : accum + 1;
 			}, 0);
-
 
 			completedCount = todos.length - activeTodoCount;
 
